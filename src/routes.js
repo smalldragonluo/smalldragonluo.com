c583100a90d2467d78dfde93b0acf8d76bc7dffb
smalldragonluo <xiaolong.lxl@alibaba-inc.com>
2018-08-02 15:18:15 +0800

    add wx sdk

--- src/routes.js
+++ src/routes.js
@@ -7,16 +7,25 @@
 
 const listService = require('./services/list');
 const wxService = require('./services/wx');
+const {logger} = require('./lib/utils');
 
 module.exports = function(app, router) {
   // page routes
   app.get('/', async function(req, res) {
+    let wx;
+
+    try {
+      wx = await wxService.getJSAPIConfig(req.protocol + '://' + req.get('host') + req.originalUrl);
+    } catch (e) {
+      logger.error(e.stack);
+    }
+
     req.query.pageSize = 100;
 
     res.render('index', {
       title: '笑话大王',
       list: await listService.getListCache(req.query),
-      wx: await wxService.getJSAPIConfig(req.protocol + '://' + req.get('host') + req.originalUrl)
+      wx: wx
     });
   });
 
